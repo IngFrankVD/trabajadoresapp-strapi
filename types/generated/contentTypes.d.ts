c3167f1f087f62bdd7c8105068a4ba8dc3e3a687
Frank <frankvd711@gmail.com>
2024-02-21 20:20:46 -0600

    agregue la tabla trabajadores

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,40 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiTrabajadorTrabajador extends Schema.CollectionType {
+  collectionName: 'trabajadores';
+  info: {
+    singularName: 'trabajador';
+    pluralName: 'trabajadores';
+    displayName: 'trabajadores';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    clave: Attribute.String & Attribute.Required & Attribute.Unique;
+    nombre: Attribute.String & Attribute.Required;
+    salario: Attribute.Decimal & Attribute.Required;
+    numeroCelular: Attribute.String;
+    lastUp: Attribute.DateTime;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::trabajador.trabajador',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::trabajador.trabajador',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -793,6 +827,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::trabajador.trabajador': ApiTrabajadorTrabajador;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::content-releases.release': PluginContentReleasesRelease;
